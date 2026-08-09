f9debe7b0ff91a7568ffb0a4457747a9ee19cc90
aarushb <Aarush.bhat04@gmail.com>
2026-08-09 01:57:28 -0600

    fix: polyfill SlowBuffer for Node 25 compatibility with jsonwebtoken

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -16,6 +16,11 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
+import buffer from "node:buffer";
+if (!buffer.SlowBuffer) {
+    (buffer as any).SlowBuffer = buffer.Buffer;
+}
+
 import type { Handle } from "@sveltejs/kit";
 import jwt from "jsonwebtoken";
 import { User } from "$lib/server/database";
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,8 @@
+import buffer from 'node:buffer';
+if (!buffer.SlowBuffer) {
+  (buffer as any).SlowBuffer = buffer.Buffer;
+}
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
